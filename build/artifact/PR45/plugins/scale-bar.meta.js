be562939f2effe63538c69a2b3f4a7aaa1e83a92
modos189 <carles3@pina.cat>
2024-02-13 05:03:23 +0000

    Update from https://github.com/modos189/website/commit/df4d412224efab2f6821c241eaa3e309b841e11b

--- build/artifact/PR45/plugins/scale-bar.meta.js
+++ build/artifact/PR45/plugins/scale-bar.meta.js
@@ -2,7 +2,7 @@
 // @author         breunigs
 // @name           IITC plugin: Scale bar
 // @category       Controls
-// @version        0.1.2.20240213.044116
+// @version        0.1.2.20240213.045818
 // @description    Show scale bar on the map.
 // @id             scale-bar
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR45/plugins/zoom-slider.meta.js
+++ build/artifact/PR45/plugins/zoom-slider.meta.js
@@ -2,7 +2,7 @@
 // @author         fragger
 // @name           IITC plugin: Zoom slider
 // @category       Controls
-// @version        0.2.2.20240213.044116
+// @version        0.2.2.20240213.045818
 // @description    Show a zoom slider on the map instead of the zoom buttons.
 // @id             zoom-slider
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
