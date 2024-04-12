fcdcaa0703370c1d2088973fbd1d42435d94f008
modos189 <carles3@pina.cat>
2024-04-12 10:47:38 +0000

    Update from https://github.com/modos189/website/commit/3df49ea2b45caf428ab89f23fea567714844df5f

--- build/artifact/PR47/plugins/pan-control.meta.js
+++ build/artifact/PR47/plugins/pan-control.meta.js
@@ -2,7 +2,7 @@
 // @author         fragger
 // @name           IITC plugin: Pan control
 // @category       Controls
-// @version        0.2.3.20240412.102351
+// @version        0.2.3.20240412.104136
 // @description    Show a panning control on the map.
 // @id             pan-control
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR47/plugins/zoom-slider.meta.js
+++ build/artifact/PR47/plugins/zoom-slider.meta.js
@@ -2,7 +2,7 @@
 // @author         fragger
 // @name           IITC plugin: Zoom slider
 // @category       Controls
-// @version        0.2.2.20240412.102351
+// @version        0.2.2.20240412.104136
 // @description    Show a zoom slider on the map instead of the zoom buttons.
 // @id             zoom-slider
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
